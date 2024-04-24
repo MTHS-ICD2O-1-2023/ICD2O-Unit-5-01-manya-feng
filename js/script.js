20f4ff04bd9dc5a5afd566a00ae6612712fc4b83
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-04-24 18:51:23 +0000

    change answer

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 // This file contains the JS functions for index.html
 
 const randomNumber = Math.floor(Math.random() * 6) + 1
+console.log(randomNumber)
 
 /**
  * This function checks if the user has selected the random number from above
@@ -17,13 +18,13 @@ function checkNumber() {
   // process
   if (userNumber == randomNumber) {
     //output
-    document.getElementById("result").innerHTML =
+    document.getElementById("answer").innerHTML =
       "You have guessed the correct number!"
   }
 
   if (userNumber != randomNumber) {
     //output
-    document.getElementById("result").innerHTML =
+    document.getElementById("answer").innerHTML =
       "You have guessed the wrong number !"
   }
 }
